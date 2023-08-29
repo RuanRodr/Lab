fb155f2ee1b00dbbc53005369208e895600ffb44
Ruan Rodrigues <141832407+RuanRodr@users.noreply.github.com>
2023-08-29 08:55:59 -0300

    Update Lab quest 15.c

--- Lab quest 15.c
+++ Lab quest 15.c
@@ -8,16 +8,16 @@ int main() {
    salario = dias * 50.25;
   
   if(dias <= 10){
-    printf("O valor do salário liquido com gratificação é: R$%.2f\n", salario);
+    printf("O valor do salário bruto com gratificação é: R$%.2f\n", salario);
   } else if(dias > 10 && dias <= 20){
     salario = salario + (salario * 20 / 100);
-    printf("O valor do salário liquido com gratificação é: R$%.2f\n", salario);
+    printf("O valor do salário bruto com gratificação é: R$%.2f\n", salario);
   } else {
     salario = salario + (salario * 30 / 100);
-    printf("O valor do salário liquido com gratificação é: R$%.2f\n", salario);
+    printf("O valor do salário bruto com gratificação é: R$%.2f\n", salario);
   }
   
   bruto = salario - (salario * 10 / 100);
-  printf("e o valor do salário Bruto é: R$%.2f", bruto);
+  printf("e o valor do salário liquido é: R$%.2f", bruto);
   return 0;
 }
